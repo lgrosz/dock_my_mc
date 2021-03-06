9388839f51c085fbdd47cd5734c95ffbc319566e
logbaseaofn <logan.grosz@gmail.com>
2021-03-06 15:55:55 -0700

    1.16.5

--- backup.sh
+++ backup.sh
@@ -10,7 +10,7 @@ fi
 echo 'say Backing started...' | sudo socat EXEC:"docker attach mc",pty STDIN
 echo 'say Expect some latency.' | sudo socat EXEC:"docker attach mc",pty STDIN
 echo 'save-off' | sudo socat EXEC:"docker attach mc",pty STDIN
-echo 'save-all fluid' | sudo socat EXEC:"docker attach mc",pty STDIN
+echo 'save-all' | sudo socat EXEC:"docker attach mc",pty STDIN
 tar -czvf backups/`date '+%Y%m%d-%H%M%S'`.tar.gz data
 echo 'save-on' | sudo socat EXEC:"docker attach mc",pty STDIN
 echo 'say Backup finished.' | sudo socat EXEC:"docker attach mc",pty STDIN
